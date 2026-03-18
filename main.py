67065ddee966f7b2238cee22eded59a20ba47a61
Jaymew luis <jaymeluis23@gmail.com>
2026-03-18 16:55:48 -0300

    teste criação tabela

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+#conexão com banco 
 import mysql.connector 
 
-conexão = mysql.connector.connect(
+banco = mysql.connector.connect(
     host= "localhost",
     port= 3306,
     user= "root",
@@ -9,7 +10,26 @@ conexão = mysql.connector.connect(
 
 )
 
-cursor = conexão.cursor()
-cursor.execute("CREATE DATABASE IF NOT EXISTS jayme_guilherme") 
+#criação do tabela 
+cursor = banco.cursor()
+cursor.execute("CREATE DATABASE IF NOT EXISTS jayme_guilherme")
+
+cursor = banco.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS usuario (id INT PRIMARY KEY, username VARCHAR(255), email VARCHAR(250), senha VARCHAR(100), idade INT)")
 
+
+#inserrção de dados do usuario 
+
+def cadastro():
+    username = input("Username: ")
+    email = input("Email: ")
+    senha = input("Senha: ")
+    idade = int(input("Idade: "))
+
+    cursor.execute(f"INSERT INTO usuario (username, email, senha, idade) VALUES ('{username}', '{email}', '{senha}', {idade})")
+    banco.commit()
+
+    print("Usuário cadastrado com sucesso!")
+
+
+cadastro()
