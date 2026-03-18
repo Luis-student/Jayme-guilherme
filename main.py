689945233d470a2d8ff1e9eac18b07c34e826fcc
Jaymew luis <jaymeluis23@gmail.com>
2026-03-18 15:07:45 -0300

    teste

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import mysql.connector 
 
-conexão = mysql.connector.connect(
+banco = mysql.connector.connect(
     host= "localhost",
     port= 3306,
     user= "root",
-    password= "Jayme-123"
+    password= "Jayme-123",
+    database= "jayme_guilherme"
 
 )
 
-cursor.execute("CREATE TABLE IF NOT EXISTS Jayme-Guilherme (id INT PRIMARY KEY, username VARCHAR(255), email VARCHAR(250), senha VARCHAR(100), idade INT)")
-
+cursor = banco.cursor()
+cursor.execute("CREATE DATABASE IF NOT EXISTS jayme_guilherme")
